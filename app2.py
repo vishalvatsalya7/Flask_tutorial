245909e71a365ff2db10c77c7e3aecbc563195ee
vishalvatsalya7 <vishalvatsalya7@gmail.com>
2020-08-23 20:52:32 +0530

    Mailing service

--- app2.py
+++ app2.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_restful.reqparse import RequestParser
 
 app = Flask(__name__)
 api = Api(app, prefix="/api/v1")
@@ -9,23 +10,51 @@ users = [
 ]
 
 
+def get_user_by_id(user_id):
+    for x in users:
+        if x.get("id") == int(user_id):
+            return x
+
+
+subscriber_request_parser = RequestParser(bundle_errors=True)
+subscriber_request_parser.add_argument("name", type=str, required=True, help="Name has to be valid string")
+subscriber_request_parser.add_argument("email", required=True)
+subscriber_request_parser.add_argument("id", type=int, required=True, help="Please enter valid integer as ID")
+
+
 class SubscriberCollection(Resource):
     def get(self):
-        return {"msg": "All subscribers "}
+        return users
 
     def post(self):
-        return {"msg": "We will create new subscribers here"}
+        args = subscriber_request_parser.parse_args()
+        users.append(args)
+        return {"msg": "Subscriber added", "subscriber_data": args}
 
 
 class Subscriber(Resource):
     def get(self, id):
-        return {"msg": "Details about user id {}".format(id)}
+        user = get_user_by_id(id)
+        if not user:
+            return {"error": "User not found"}
+
+        return user
 
     def put(self, id):
-        return {"msg": "Update user id {}".format(id)}
+        args = subscriber_request_parser.parse_args()
+        user = get_user_by_id(id)
+        if user:
+            users.remove(user)
+            users.append(args)
+
+        return args
 
     def delete(self, id):
-        return {"msg": "Delete user id {}".format(id)}
+        user = get_user_by_id(id)
+        if user:
+            users.remove(user)
+
+        return {"message": "Deleted"}
 
 
 api.add_resource(SubscriberCollection, '/subscribers')
